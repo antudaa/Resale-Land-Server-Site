b1f3bfdbebb953730f0947773c62c0f236b806b1
Antu_Das <antuchandradasp9@gmail.com>
2022-11-27 06:19:47 -0800

    Implemented Product category API

---
+++ category.js
@@ -0,0 +1,256 @@
+[
+    {
+      "categoryId": 1,
+      "category": "car",
+      "productName": "Range Rover Sport 2016",
+      "img": [
+        "https://www.autobest.co.in/uploads/blog/706126190152.jpeg",
+        "https://ag-spots-2017.o.auroraobjects.eu/2017/05/10/land-rover-range-rover-sport-svr-c657910052017221600_2.jpg",
+        "https://www.wrapstyle.com/content/img_cache/800x450/1595335225-Range-Rover-SVR-Grey-Matt-wrap-4-.jpg",
+        "https://www.wrapstyle.com/content/img_cache/1920x/1596527576-1543-Range-Rover-SVR-Grey-Matt-wrap-6-.jpeg",
+        "https://cargiant.co.in/uploads/car/181215419369.jpeg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/2018_Land_Rover_Range_Rover_Velar_D180_HSE_Interior.jpg/800px-2018_Land_Rover_Range_Rover_Velar_D180_HSE_Interior.jpg?20190117213808",
+        "https://imgd.aeplcdn.com/0x0/n/cw/ec/28085/landrover-range-rover-velar-interior6.jpeg",
+        "https://www.compararcoche.com/wp-content/uploads/2022/08/Land-rover-range-rover-velar-interior.jpg"
+      ],
+      "location": "Dhaka",
+      "price": 6700000,
+      "milage": "45000 Kilometers",
+      "originalPrice": 15200000,
+      "usedTime": "2 Years",
+      "postDate": "07-09-2022",
+      "sellerInfo": {
+        "name": "Nayon Das.",
+        "verified": true
+      }
+    },
+    {
+      "categoryId": 1,
+      "category": "car",
+      "productName": "Hyundai",
+      "img": [
+        "https://hips.hearstapps.com/roa.h-cdn.co/assets/16/24/1466110023-hyundai-equus-2011-1600-0a.jpg",
+        "https://i0.wp.com/newportconvertible.com/wp-content/uploads/2012/04/14Equus1100NSC-td2A10-20-1.jpg?fit=1024%2C593&ssl=1",
+        "https://www.team-bhp.com/forum/attachments/international-automotive-scene/199740d1254281613-hyundais-most-expensive-expansive-car-new-2010-equus-revealed-160724.jpg",
+        "https://fijianchick.files.wordpress.com/2013/04/shereen-shabnam-2014-hyundai-centennial.jpg",
+        "https://fijianchick.files.wordpress.com/2013/04/shereen-2014-hyundai-centennial.jpg?w=848",
+        "https://i.pinimg.com/originals/21/91/98/219198c8085268ece0d840a5d8190848.jpg",
+        "https://www.gannett-cdn.com/-mm-/a84d68b642cd97a0de69a4273fc84851e2f53932/c=793-87-1310-379/local/-/media/USATODAY/test/2013/10/16/1381952517000-hyundai.jpg"
+      ],
+      "location": "Chittagong",
+      "price": 5000000,
+      "milage": "25000 Kilometers",
+      "originalPrice": 9200000,
+      "usedTime": "1 Years",
+      "postDate": "14-09-2022",
+      "sellerInfo": {
+        "name": "Ayon Das.",
+        "verified": true
+      }
+    },
+    {
+      "categoryId": 1,
+      "category": "car",
+      "productName": "Jaguar XF",
+      "img": [
+        "https://5.imimg.com/data5/GS/WF/GN/SELLER-1705300/jaguar-luxury-car-in-bengaluru-500x500.jpg",
+        "https://img.indianauto.com/crop/459x287/2021/02/07/ff3a13aa-a47f-4f1d-87ad-00d7715d1667-9d85.jpg",
+        "https://img.indianauto.com/crop/459x287/2018/12/03/cd8dcb61-c134-42d2-8979-09133b3644a1-2c1d.jpg",
+        "https://image-cdn.beforward.jp/large/202011/2140000/BH608529_4bdef6.jpg",
+        "https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/0_am3lpwhz/def_height/2700/def_width/2700/version/100012/type/1",
+        "https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/media/51/jaguar-xj-ultimate-inline-a-photo-453622-s-original.jpg",
+        "https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/media/51/jaguar-xj-ultimate-inline-b-photo-453623-s-original.jpg",
+        "https://f7432d8eadcf865aa9d9-9c672a3a4ecaaacdf2fee3b3e6fd2716.ssl.cf3.rackcdn.com/C2846/U230/IMG_10058-mini.jpg",
+        "https://i.pinimg.com/originals/ac/94/0a/ac940ab0e95c0f96985567308f3cb3d3.jpg",
+        "https://f7432d8eadcf865aa9d9-9c672a3a4ecaaacdf2fee3b3e6fd2716.ssl.cf3.rackcdn.com/C2846/U230/IMG_10058-mini.jpg"
+      ],
+      "location": "Banani",
+      "price": 9200000,
+      "milage": "38000 Kilometers",
+      "originalPrice": 12500000,
+      "usedTime": "2 Years",
+      "postDate": "15-09-2022",
+      "sellerInfo": {
+        "name": "Antu Das.",
+        "verified": true
+      }
+    },
+    {
+      "categoryId": 1,
+      "category": "car",
+      "productName": "Audi",
+      "img": [
+        "https://gumlet.assettype.com/dtnext/import/Articles/2021/Nov/202111010153543237_High-taxes-hampering-growth-of-luxury-car-segment-in-India_SECVPF.gif?w=1200&h=675&auto=format%2Ccompress&fit=max",
+        "https://kingsauto.in/Admin/img/1603721791image1.png",
+        "https://imgd.aeplcdn.com/640X480/cw/ucp/stockApiImg/Z555LAH_27305a8259a4477890b0a54648e41171_1_25102173.jpg?q=75",
+        "https://i.pinimg.com/736x/f2/94/42/f29442f4697e5939c3d0dac492f02dfa.jpg",
+        "https://images.mid-day.com/images/images/2016/oct/Audi-A4-2.jpg",
+        "https://i2-prod.cambridge-news.co.uk/incoming/article12297998.ece/ALTERNATES/s615/Audi-lights-2.jpg",
+        "https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i2320526/2016_audi_tt_coupe_rearseat.jpg",
+        "https://www.automobile-magazine.fr/asset/cms/840x394/6855/config/6530/seul-le-tt-rs-coupe-peut-accueillir-des-passagers-arriere.jpg"
+      ],
+      "location": "Narayangong",
+      "price": 4500000,
+      "milage": "20000 Kilometers",
+      "originalPrice": 6800000,
+      "usedTime": "1 Years",
+      "postDate": "19-10-2022",
+      "sellerInfo": {
+        "name": "Nirjhor Saha",
+        "verified": true
+      }
+    },
+    {
+      "categoryId": 1,
+      "category": "car",
+      "productName": "Mercedes S Class",
+      "img": [
+        "https://www.autobest.co.in/uploads/car/022662213502.jpg",
+        "https://www.luxurycarrental.ae/wp-content/uploads/2021/06/mercedes-e300-luxury-rent-a-car-1.jpg",
+        "https://renty.ae/cdn-cgi/image/format=auto,fit=contain/https://renty.ae/uploads/car/photos/l/white_mercedes-e-class_2019_10033_cc02c53f64721d30020b18af709f7f97.jpg",
+        "https://comfortmytravel.com/wp-content/uploads/2016/10/FullSizeRender-640x400.jpg",
+        "https://carsemsar.s3.amazonaws.com/cars/2022/06/19/8278187984674793/62af0664061d65.36542549/photo-sm.jpg",
+        "https://ymimg1.b8cdn.com/resized/car_model/1574/pictures/1244946/listing_main_2014_Mercedes-Benz_E_Class_Saloon_Steering.jpg",
+        "https://ymimg1.b8cdn.com/resized/car_model/803/pictures/308096/listing_main_mercedes-benz-e-class-saloon-w212_wallpaper_08_1600x1200_03-2013.jpg",
+        "https://cars.usnews.com/static/images/Auto/izmo/326054/2011_mercedes_benz_e_class_frontseat.jpg",
+        "https://cars.usnews.com/static/images/Auto/izmo/326054/2011_mercedes_benz_e_class_rearseat.jpg",
+        "https://cars.usnews.com/static/images/Auto/izmo/i159614063/2020_mercedes_benz_e_class_trunk.jpg"
+      ],
+      "location": "Chittagong",
+      "price": 7200000,
+      "milage": "35000 Kilometers",
+      "originalPrice": 13000000,
+      "usedTime": "1.5 Years",
+      "postDate": "24-10-2022",
+      "sellerInfo": {
+        "name": "Satirtha Sen",
+        "verified": true
+      }
+    },
+    {
+      "categoryId": 2,
+      "category": "mobile",
+      "productName": "Oneplus 7t",
+      "img": [
+        "https://cdn.mos.cms.futurecdn.net/omzFSREW6uD4JErmtcvndD.jpg",
+        "https://i.ytimg.com/vi/zgyLCPwmmTY/maxresdefault.jpg",
+        "https://assets.mspimages.in/wp-content/uploads/2019/10/Oneplus-7T-Unboxing-Accessories.jpeg",
+        "https://i.ytimg.com/vi/aK9RvkMhwHI/maxresdefault.jpg",
+        "https://content.fortune.com/wp-content/uploads/2019/09/DSCF1882-e1569506731332.jpg",
+        "https://i.guim.co.uk/img/media/17f4fcce97af5df64be4c949200cc148030007e6/335_399_4219_2533/master/4219.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=fc21b1c08a8d85314668f8ed5e0365ea",
+        "https://thurrott.s3.amazonaws.com/2019/10/op7t-hero.jpg"
+      ],
+      "location": "Chittagong",
+      "price": 28000,
+      "originalPrice": 34000,
+      "usedTime": "8 Months",
+      "postDate": "19-10-2022",
+      "sellerInfo": {
+        "name": "Nirjhor Saha",
+        "verified": true
+      }
+    },
+    {
+      "categoryId": 2,
+      "category": "mobile",
+      "productName": "Iphone XS",
+      "img": [
+        "https://media.karousell.com/media/photos/products/2020/11/9/iphone_xs_64gb_gold_1604897052_36e149ab.jpg",
+        "https://d46-a.sdn.cz/d_46/c_img_gV_u/k0RBhm.jpeg?fl=exf|crr,1.33333,2|res,1024,768,1|wrm,/watermark/sbazar.png,10,10|jpg,80,,1",
+        "https://webimg.secondhandapp.at/1.1/5c7babdf772f911e14ed2d25",
+        "https://media.karousell.com/media/photos/products/2022/3/5/iphone_xs_gold_64gb_1646464404_881dc8f6.jpg"
+      ],
+      "location": "Dhaka",
+      "price": 32000,
+      "originalPrice": 50000,
+      "usedTime": "1.5 Years",
+      "postDate": "28-10-2022",
+      "sellerInfo": {
+        "name": "Jhankar Mahabub",
+        "verified": true
+      }
+    },
+    {
+      "categoryId": 2,
+      "category": "mobile",
+      "productName": "Mi Note 11",
+      "img": [
+        "https://media.karousell.com/media/photos/products/2021/7/18/used_mi_11_5g_128gb_grey_1626574230_6eff2ed9_progressive.jpg",
+        "https://s3-eu-west-1.amazonaws.com/img4.haraj.com.sa/cache4/348x464-1_-uTSogmxi4e4iYl.jpg",
+        "https://media.karousell.com/media/photos/products/2021/7/18/used_mi_11_5g_128gb_grey_1626574230_6eff2ed9_progressive.jpg",
+        "https://www.mobiles4sale.in/photos-2/126/126595/xiaomi-mi-11-lite-1627114488.jpg",
+        "https://i.gadgets360cdn.com/large/mi_11x_bottom_ndtv_1619707119796.jpg"
+      ],
+      "location": "Shyamoli",
+      "price": 26000,
+      "originalPrice": 40000,
+      "usedTime": "8 Months",
+      "postDate": "10-10-2022",
+      "sellerInfo": {
+        "name": "Sakib Uddin",
+        "verified": true
+      }
+    },
+    {
+      "categoryId": 3,
+      "category": "laptop",
+      "productName": "Dell XPS 13",
+      "img": [
+        "https://m.media-amazon.com/images/I/81S-yXzvFPL.jpg",
+        "https://stadt-bremerhaven.de/wp-content/uploads/2020/05/xps-15-dell.jpg",
+        "https://laptopworld.vn/media/product/9857_dell_xps_17_9710_2.jpg",
+        "https://preview.redd.it/uiqwhf6dbgw41.jpg?width=3809&format=pjpg&auto=webp&s=cb7b379a49091486670673e1afe0dee798b62160",
+        "https://reviewed-com-res.cloudinary.com/image/fetch/s--Yam_VfVT--/b_white,c_limit,cs_srgb,f_auto,fl_progressive.strip_profile,g_center,h_668,q_auto,w_1187/https://reviewed-production.s3.amazonaws.com/1461089091000/dell-xps-13-2016-beauty.jpg"
+      ],
+      "location": "Dhaka",
+      "price": 82000,
+      "originalPrice": 220000,
+      "usedTime": "1.5 Years",
+      "postDate": "13-10-2022",
+      "sellerInfo": {
+        "name": "Habibullah Bahar",
+        "verified": true
+      }
+    },
+    {
+      "categoryId": 3,
+      "category": "laptop",
+      "productName": "HP",
+      "img": [
+        "https://media.karousell.com/media/photos/products/2022/6/22/hp_probook_440_g5_used_1655875838_098ca673_progressive.jpg",
+        "https://media.karousell.com/media/photos/products/2021/8/8/hp_probook_440_g7_intel_core_i_1628381985_7bb1249f_progressive.jpg",
+        "https://www.megabites.com.ph/wp-content/uploads/2021/04/DSCF0995.jpg",
+        "https://www.megabites.com.ph/wp-content/uploads/2021/04/hp-probook.jpg",
+        "https://www.megabites.com.ph/wp-content/uploads/2021/04/HP-ProBook-445-G7-2.jpg"
+      ],
+      "location": "Dhaka",
+      "price": 32000,
+      "originalPrice": 52000,
+      "usedTime": "6 Months",
+      "postDate": "13-10-2022",
+      "sellerInfo": {
+        "name": "Rahim Uddin.",
+        "verified": true
+      }
+    },
+    {
+      "categoryId": 3,
+      "category": "laptop",
+      "productName": "ASUS",
+      "img": [
+        "https://i5.walmartimages.com/asr/f998cd52-b657-43f2-967a-ac2d6b58afe0.f9a5467a88f3eb4919e6578395f0e294.jpeg",
+        "https://goprice.com.bd/wp-content/uploads/2021/07/Asus-ZenBook-14-UX425EA-Core-i5-price.jpg",
+        "https://www.hitechcentury.com/wp-content/uploads/2020/11/20201122_001547-01_compress78-1024x576.jpg",
+        "https://1.bp.blogspot.com/-_SVz-3mlfQs/Xy18WuzOXKI/AAAAAAAAr5o/XH7z7IyG5YYWjUK5t_z3rxxppBUdTDlVQCLcBGAsYHQ/s1600/asus-zenbook-14-ux425-specs-price-review-philippines.webp"
+      ],
+      "location": "Sylhet",
+      "price": 42000,
+      "originalPrice": 80000,
+      "usedTime": "1 Years",
+      "postDate": "22-10-2022",
+      "sellerInfo": {
+        "name": "Raihan Uddin",
+        "verified": true
+      }
+    }
+  ]
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 
 const app = express();
@@ -12,6 +14,62 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Resale Land Is Running");
 });
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p4iytrv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    try {
+
+        const categoryCollection = client.db('ResaleLand').collection('categoryCollection');
+        const userCollection = client.db('ResaleLand').collection('userCollection');
+        const productCollection = client.db('ResaleLand').collection('productsCollection');
+
+
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const result = await categoryCollection.find(query).toArray();
+            res.send(result);
+        });
+
+
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body;
+            const result = await userCollection.insertOne(userInfo);
+            res.send(result);
+        });
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const result = await productCollection.find(query).toArray();
+            res.send(result);
+        });
+
+
+        app.get('/category/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = {
+                categoryId: ('id')
+            };
+            const product = await productCollection.find(query).toArray();
+            res.send(product);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.log);
+
+
 
 
 app.listen(port, () => console.log(`Server Running on Port ${port}`));
